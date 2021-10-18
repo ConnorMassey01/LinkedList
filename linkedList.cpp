ab98846c6842855bcc79f5df5f0a64c14b8962c7
Connor Massey <84544538+ConnorMassey01@users.noreply.github.com>
2021-10-18 11:48:31 -0400

    Updated linkedList.cpp by adding comments

--- linkedList.cpp
+++ linkedList.cpp
@@ -1,23 +1,16 @@
 #include <iostream>
+#include "linkedList.h"
 using namespace std;
 
-struct node{
-	int data;
-	struct node *next;
-	struct node *prev;
-};
-
-struct linkedList{
-	struct node *head = NULL;
-	struct node *tail = NULL;
-};
-
 void insert(int d, struct linkedList *l){
-	struct node *n = (struct node *)malloc(sizeof(struct node));
+	//allocate memory to create pointer to node struct
+	struct node* n = (struct node *)malloc(sizeof(struct node));
+	//exit if node was not created
 	if (n == nullptr){
 		cout << "error\n";
 		exit(1);
 	}
+	//fit the node to the front of the linked list
 	n->data = d;
 	n->next = NULL;
 	n->prev = NULL;
@@ -27,16 +20,19 @@ void insert(int d, struct linkedList *l){
 	}
     else{
         l->head->prev = n;
-        n -> next = l->head;
+        n->next = l->head;
         l->head = n;
     }
 }
 
 int pull(struct linkedList *l){
+	//return 0 if there is nothing in the list to pull
 	if(l->head == nullptr){
 		return 0;
 	}
-	struct node *n = l->head;
+	//take the node from the start of the list
+	struct node* n = l->head;
+	//assign the head to point to the next node and properly repair the linked list
 	l->head = l->head->next;
 	if(l->head == nullptr){
       	l->tail = NULL;
@@ -44,17 +40,19 @@ int pull(struct linkedList *l){
     else{
         l->head->prev = NULL;
     }
-
-	int retval = n -> data;
+	//take the data from the node pointing to what was the head of the list and then free the memory
+	int retval = n->data;
     free(n);
     return retval;
 }
 
 void printList(struct linkedList *l){
+	//return if there is no list to print
 	if(l->head == nullptr){
 		return;
 	}
-	struct node *n = l->head;
+	//start at the head of the list and print each node's data until the end of the list
+	struct node* n = l->head;
 	while(n != nullptr){
 		if(n->next == nullptr){
 			cout << n->data << "\n";
