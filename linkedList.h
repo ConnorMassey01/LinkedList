20e2dac748800190e1ce2f4b82cedc3e9687cc9a
Connor Massey <84544538+ConnorMassey01@users.noreply.github.com>
2021-10-18 11:49:57 -0400

    Updated linkedList.h by adding comments

--- linkedList.h
+++ linkedList.h
@@ -9,7 +9,10 @@ struct linkedList{
 	struct node *tail = NULL;
 };
 
+//insert an element to the front of the linked list
 void insert(int d, struct linkedList *l);
+//pull an element from the front of the linked list
 int pull(struct linkedList *l);
+//print the list from the head to the tail
 void printList(struct linkedList *l);
 
